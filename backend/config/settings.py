93ed15c4c445e30f49ac9193e4fcd3d0f808832a
Ghost-fjr <faizashrafmuhamed99@gmail.com>
2026-07-17 17:43:45 +0300

    fix: correctly replace STORAGES and avoid DEFAULT_FILE_STORAGE mutual exclusivity crash

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -202,11 +202,25 @@ STORAGES = {
     },
 }
 
+# If Cloudinary is configured, use it for media storage
+if config('CLOUDINARY_CLOUD_NAME', default=''):
+    STORAGES["default"]["BACKEND"] = 'cloudinary_storage.storage.MediaCloudinaryStorage'
+
 MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
 # Cloudinary Settings (Production Media Storage)
 CLOUDINARY_STORAGE = {
+    'CLOUD_NAME': config('CLOUDINARY_CLOUD_NAME', default=''),
+    'API_KEY': config('CLOUDINARY_API_KEY', default=''),
+    'API_SECRET': config('CLOUDINARY_API_SECRET', default=''),
+}
+
+# Default primary key field type
+# https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
 # File Upload Settings
 FILE_UPLOAD_MAX_MEMORY_SIZE = 5242880  # 5MB
 DATA_UPLOAD_MAX_MEMORY_SIZE = 5242880  # 5MB
